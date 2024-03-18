f064c79155052971b7af997554474cc5e99aa86b
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-03-18 19:45:59 +0100

    ...

--- number_guess.py
+++ number_guess.py
@@ -10,18 +10,13 @@ def guessing():
     guesses = 0
     
     while True:
-        
         guess = int(input("Guess the number! ")) 
         guesses += 1
         if guess > rand:
             print("That's high. Bring it down a little.")
-            # guesses += 1
-            # print(guesses += 1)
         elif guess < rand:
             print("You're close. Go a bit higher.'")
-            # guesses += 1
         else:
-            # guesses += 1
             print("You got it in {} guesses!!".format(guesses))
             play = input("would you like to play again? (y/n) ")
             if play.lower() != 'y':
